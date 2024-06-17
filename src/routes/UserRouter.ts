8badcbb08f77620116f35655d71eb00b51fac1f4
ekimcoskun <ekim.coskun19@gmail.com>
2024-06-17 18:13:24 +0300

    feat: Add Hash and Token interfaces and implementations
    - Added IHash interface and Hash class implementation for password hashing and comparison
    - Added IToken interface and Token class implementation for token generation and verification
    - Updated UserRouter to bind Hash and Token dependencies

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -6,6 +6,8 @@ import { UserService } from '../services/UserService';
 import { IUserRepository } from '../interfaces/User/IUserRepository';
 import { UserRepository } from '../repositories/UserRepository';
 import { UserController } from '../controllers/UserController';
+import { Token } from '../libs/token';
+import { Hash } from '../libs/hash';
 
 const userRouter = express.Router();
 
@@ -13,8 +15,9 @@ const container = new Container();
 
 container.bind<IUserService>(INTERFACE_TYPE.UserService).to(UserService);
 container.bind<IUserRepository>(INTERFACE_TYPE.UserRepository).to(UserRepository);
-
 container.bind(INTERFACE_TYPE.UserController).to(UserController)
+container.bind(INTERFACE_TYPE.Token).to(Token);
+container.bind(INTERFACE_TYPE.Hash).to(Hash);
 
 const controller = container.get<UserController>(INTERFACE_TYPE.UserController);
 
--- src/utils/appContsts.ts
+++ src/utils/appContsts.ts
@@ -2,5 +2,7 @@
 export const INTERFACE_TYPE= {
     UserService: Symbol.for("IUserService"),
     UserRepository: Symbol.for("IUserRepository"),
-    UserController: Symbol.for("IUserController")
+    UserController: Symbol.for("IUserController"),
+    Token: Symbol.for("IToken"),
+    Hash: Symbol.for("IHash")
 }
