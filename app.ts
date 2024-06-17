f840d1afb23be10e935f536b41c9ef983e550f4b
ekimcoskun <ekim.coskun19@gmail.com>
2024-06-17 19:47:32 +0300

    feat: Add Http and Logs on middleware
    - Add /.logs to .gitignore
    - Add body-parser and cors dependencies to package.json
    - Add middleware for request and logs

--- app.ts
+++ app.ts
@@ -2,8 +2,12 @@ import express from 'express';
 import "reflect-metadata"
 import { connect } from "./src/helpers/connectDB";
 import userRouter from './src/routes/UserRouter';
+import Http from './src/middlewares/Http';
 
 const app = express();
+
+Http.mount(app);
+
 app.use(express.json());
 
 app.use("/api/user", userRouter)
