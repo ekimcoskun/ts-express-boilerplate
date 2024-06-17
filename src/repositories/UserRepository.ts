4e08f00336c5f67c8b7c8ba12c6a7b54a54a34f9
ekimcoskun <ekim.coskun19@gmail.com>
2024-06-17 18:57:35 +0300

    refactor: Update UserRepository methods to use Mongoose models

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,22 +1,47 @@
 import { injectable } from "inversify";
 import { User } from "../entities/User";
 import { IUserRepository } from "../interfaces/User/IUserRepository";
+import UserModel from "../models/User";
 
 @injectable()
 export class UserRepository implements IUserRepository {
-    login(email: string): Promise<User> {
-        throw new Error("Method not implemented.");
+    async login(email: string): Promise<User> {
+        const user = await UserModel.findOne({ email: email });
+        if (!user) {
+            throw new Error("User not found");
+        } else {
+            return user;
+        }
     }
-    register(name: string, email: string, password: string, phoneNumber: string): Promise<string> {
-        throw new Error("Method not implemented.");
+    async register(name: string, email: string, password: string, phoneNumber: string): Promise<string> {
+        const user = new UserModel({ name, email, password, phoneNumber });
+        const result = await user.save();
+        if (!result) {
+            throw new Error("User not created");
+        }
+        return "User created";
     }
-    getAllUsers(): Promise<User[]> {
-        throw new Error("Method not implemented.");
+    async getAllUsers(): Promise<User[]> {
+        const users = await UserModel.find();
+        if (!users) {
+            return [];
+        } else {
+            return users;
+        }
     }
-    deleteUser(id: string): Promise<string> {
-        throw new Error("Method not implemented.");
+    async deleteUser(id: string): Promise<string> {
+        const result = await UserModel.deleteOne({ id });
+        if (result.deletedCount === 0) {
+            throw new Error("User not deleted");
+        }
+        return "User deleted";
     }
-    updateUser(id: string, user: User): Promise<string> {
-        throw new Error("Method not implemented.");
+    async updateUser(id: string, user: User): Promise<string> {
+        const result = await UserModel.findByIdAndUpdate(id, user);
+        if (!result) {
+            throw new Error("User not updated");
+        } else {
+            return "User updated";
+        }
     }
 }
