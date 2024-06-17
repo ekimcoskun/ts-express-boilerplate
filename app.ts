4d089f0bd0f49febcd33419f51ce0baf536c3295
ekimcoskun <ekim.coskun19@gmail.com>
2024-06-17 18:41:49 +0300

    feat: Add database connection function

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import "reflect-metadata"
+import { connect } from "./src/helpers/connectDB";
+
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+    await connect();
     console.log('Server is running on port 3000');
 });
